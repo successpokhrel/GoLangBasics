44e75ba6a34d556decf51c38f056140083f5f164
successpokhrel <successpokhrel25@gmail.com>
2024-12-13 13:44:01 +0545

    print formatter

--- main.go
+++ main.go
@@ -4,22 +4,17 @@ import "fmt"
 
 func main() {
 
-	//ints
-	var ageOne int = 20
-	var ageTwo = 30
-	ageThree := 40
+	//print formatting
+	age := "12"
+	name := "Thito"
 
-	fmt.Println(ageOne, ageTwo, ageThree)
+	fmt.Printf("His name is %v and his age is %v\n", age, name)
+	fmt.Printf("His name is %q and his age is %q\n", age, name)
+	fmt.Printf("age is of type %T\n", age)
+	fmt.Printf("you scored %f points!\n", 225.55)
+	fmt.Printf("you scored %0.1f points! \n", 225.55)
 
-	//bits and memory
-	var numOne int8 = 25
-	var numTwo int8 = -128
-	var numThree uint16 = 256
-
-	fmt.Println(numOne, numTwo, numThree)
-
-	var scoreOne float32 = 25.98
-	var scoreTwo float64 = 686454654.684465
-
-	fmt.Println(scoreOne, scoreTwo)
+	//Sprintf (save formatted strings)
+	var str = fmt.Sprintf("His name is %v and his age is %v\n", age, name)
+	fmt.Println("saved: ", str)
 }
