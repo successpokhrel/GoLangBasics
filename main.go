80b2b0727937ce2a2ec2388dff512aeeb5055e11
successpokhrel <successpokhrel25@gmail.com>
2024-12-13 16:07:18 +0545

    structs and custom types

--- main.go
+++ main.go
@@ -2,26 +2,10 @@ package main
 
 import "fmt"
 
-func updateName(x string) {
-	x = "dewge"
-}
-
-func updateNameTruePointer(x *string) {
-	*x = "dewge"
-}
-
 func main() {
-	// group A types -> strings, ints, bools, floats, arrays, structs
-	name := "fita"
+	mybill := newBill("success's bill")
 
-	updateName(name)
-
-	m := &name
-	fmt.Println("memory address: ", m)
-	fmt.Println("value at memory address: ", *m)
-
-	updateNameTruePointer(m)
-
-	fmt.Println("memory address: ", m)
-	fmt.Println("value at memory address: ", *m)
+	fmt.Println(mybill)
 }
+
+// go run main.go bill.go
