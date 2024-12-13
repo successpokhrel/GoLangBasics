7f0ffd480843dc134f864c56bf8fc3149d6ee32f
successpokhrel <successpokhrel25@gmail.com>
2024-12-13 15:20:20 +0545

    conditionals

--- main.go
+++ main.go
@@ -3,32 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	// x := 0
-	// for x < 5 {
-	// 	fmt.Println("X is: ", x)
-	// 	x++
-	// }
 
-	// for i := 0; i < 5; i++ {
-	// 	fmt.Println("value of i is: ", i)
-	// }
+	names := []string{"mario", "lungi", "yoshi", "peach", "bowser"}
 
-	names := []string{"mario", "lungi", "yoshi", "peach"}
+	for index, value := range names {
+		if index == 1 {
+			fmt.Println("continuing at pos", index)
+			continue
+		}
+		if index > 2 {
+			fmt.Println("breaking at pos", index)
+			break
+		}
 
-	// for i := 0; i < len(names); i++ {
-	// 	fmt.Println(names[i])
-	// }
-
-	// for index, value := range names {
-	// 	fmt.Printf("the value at index %v is %v \n", index, value)
-	// }
-
-	for _, value := range names {
-		fmt.Printf("the value is %v \n", value)
-		//this doesnot update the value in original slice.
-		value = "new string"
+		fmt.Printf("the value at pos %v is %v \n", index, value)
 	}
-
-	fmt.Println(names)
 
 }
