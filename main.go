1a987c4a18ea6aed4aab37ccc26cea33407127b8
successpokhrel <successpokhrel25@gmail.com>
2024-12-13 13:31:20 +0545

    Strings

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//strings
+	var stringOne string = "Tom"
+	var stringTwo = "Jerry"
+	var stringThree string
+
+	fmt.Println(stringOne, stringTwo, stringThree)
+
+	stringOne = "Success"
+	stringThree = "Pokhrel"
+
+	fmt.Println(stringOne, stringTwo, stringThree)
+
+	//shorthand syntax
+	nameFour := "Biththal"
+	fmt.Println(nameFour)
+}
