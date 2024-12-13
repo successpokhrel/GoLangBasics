1f59c67306190ded17d0a7e9b0fe549fa555f77f
successpokhrel <successpokhrel25@gmail.com>
2024-12-13 13:35:09 +0545

    ints and floats

--- main.go
+++ main.go
@@ -4,19 +4,22 @@ import "fmt"
 
 func main() {
 
-	//strings
-	var stringOne string = "Tom"
-	var stringTwo = "Jerry"
-	var stringThree string
+	//ints
+	var ageOne int = 20
+	var ageTwo = 30
+	ageThree := 40
 
-	fmt.Println(stringOne, stringTwo, stringThree)
+	fmt.Println(ageOne, ageTwo, ageThree)
 
-	stringOne = "Success"
-	stringThree = "Pokhrel"
+	//bits and memory
+	var numOne int8 = 25
+	var numTwo int8 = -128
+	var numThree uint16 = 256
 
-	fmt.Println(stringOne, stringTwo, stringThree)
+	fmt.Println(numOne, numTwo, numThree)
 
-	//shorthand syntax
-	nameFour := "Biththal"
-	fmt.Println(nameFour)
+	var scoreOne float32 = 25.98
+	var scoreTwo float64 = 686454654.684465
+
+	fmt.Println(scoreOne, scoreTwo)
 }
